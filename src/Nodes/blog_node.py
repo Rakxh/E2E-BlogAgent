5d7d8813a7d43a685bc825687adc29135b9ce0ba
Rakesh Kumar Jena <rakeshjena.kumar02@gmail.com>
2026-07-09 23:48:26 +0530

    Final Commit

--- src/Nodes/blog_node.py
+++ src/Nodes/blog_node.py
@@ -1,4 +1,8 @@
+from glob import translate
 from src.State.blogstate import BlogState
+from langchain_core.messages import HumanMessage, SystemMessage
+from src.State.blogstate import Blog
+
 class BlogNode:
     """
     A class to represent blog node
@@ -29,3 +33,40 @@ class BlogNode:
             response=self.llm.invoke(sys_message)
             return {"blog":{"title":state["blog"]['title'],'content':response.content}}
     
+
+    def translation(self,state:BlogState):
+        """
+        Translate the content to the specific language
+        """
+
+        translate_prompt="""
+                         Translate the following content into {current_language}:
+                         - Maintain the original tone, style and formatting.
+                         - Adapt cultural references and idioms to be appropriate for {current_language}
+
+                         ORIGINAL CONTENT:
+                         {blog_content}
+
+        """
+        blog_content=state["blog"]["content"]
+
+        messages=[HumanMessage(translate_prompt.format(current_language=state['current_language'],blog_content=blog_content))]
+
+        translate_content=self.llm.with_structured_output(Blog).invoke(messages)
+
+    def route(self,state):
+        return{"current_language":state["current_language"]}
+    
+    def route_decision(self,state):
+        """
+        route the content to the respective translation function
+
+        """
+        if state['current_language']=='hindi':
+            return 'hindi'
+        
+        elif state['current_language']=='french':
+            return 'french'
+        
+        else:
+            return state['current_language']
